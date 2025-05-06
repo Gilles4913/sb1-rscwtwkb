cf57170458c914cbe6d44469ce9229edd1c79976
Gilles4913 <gilles.guerrin@a2display.fr>
2025-05-06 10:00:14 +0200

    Update ScrollToTop.tsx

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,11 +1,22 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
+declare global {
+  interface Window {
+    gtag: (key: string, ...rest: any[]) => void;
+  }
+}
+
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+    
+    // Send page view to Google Analytics
+    window.gtag('event', 'page_view', {
+      page_path: pathname,
+    });
     
     // Update meta tags based on current route
     const routeMetaTags = {
